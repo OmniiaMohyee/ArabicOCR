517899a68e10e0f0582a6ed6b7e9d52deaa7f680
OmniiaMohyee <zicomohyee@yahoo.com>
2019-12-20 13:28:28 +0200

    fix word segmentation

--- segmentation/word_segment_2.py
+++ segmentation/word_segment_2.py
@@ -32,7 +32,7 @@ def line_segment(image,not_bin_image):
         result_lines.append(im[bounds[i]+1:bounds[i+1],:])
         
         result_lines_not_bin.append(im_2[bounds[i]+1:bounds[i+1],:])
-        # cv2.imwrite("line"+str(line)+".png",im_2[bounds[i]+1:bounds[i+1],:])
+        cv2.imwrite("line"+str(line)+".png",im_2[bounds[i]+1:bounds[i+1],:])
         line+=1
     return result_lines,result_lines_not_bin,line-1
 
@@ -73,19 +73,23 @@ def word_segment(image,not_bin_image,threshold,scale):
         result_words.append(result)
         word += 1
 
-    return result_words,word-1
-
+    return result_words,word-1,im
 
 
 def word_seg(clean_img,clean_img_not_bin):
+    
     lines, not_bin_lines, size_lines = line_segment(clean_img,clean_img_not_bin)
-    print(size_lines)
     all_words = []
     count  = 0 
     for j in range(size_lines):
-        words,size_words = word_segment(lines[j],not_bin_lines[j],10,5)# 1--> threshold : hyperparamter
-        all_words += words
-        # for i in range(size_words):
-            # cv2.imwrite("word"+str(count)+".png",words[i])   
+        words,length_words,im = word_segment(lines[j],not_bin_lines[j],5,10)# 1--> threshold : hyperparamter
+        print(length_words)
+        count+= length_words
+        #cv2.imwrite("line"+str(j+1)+".png",im)
+    #     for i in range(size_words):
+            #cv2.imwrite("word"+str(count)+".png",words[i])
             #count += 1
-    return all_words
+    print(count)
+
+img,not_bin_img = preproc("tests/capr3.png")
+word_seg(img,not_bin_img)
